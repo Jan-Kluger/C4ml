f8d9fe435ced87a5f5e56e3ef29cf9d7ff4ff8b3
yannick <yannick@schuermann.org>
2025-03-16 23:56:14 +0100

    worked on main method

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,1 +1,14 @@
+open C4_lib.Player_sig
+
+(* Functor takes in two players to play game of connect 4 *)
+module GAME (A : PLAYER_SIG) (B : PLAYER_SIG) = struct
+  let make_move (state : chip array array) : int =
+    (* Initialize with an empty playing field *)
+    let state = Array.init 7 (fun _ -> Array.make 7 Free) in
+
+
+    (* Temporarily return -1 *)
+    (-1)
+end
+
 let () = print_endline "Hello, World!"
