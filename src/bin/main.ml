9f42c6bd359c62af9340b2b74f3a223bda6e1379
yannick <yannick@schuermann.org>
2025-03-17 11:35:44 +0100

    finished turn engine

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -8,9 +8,8 @@ module GAME (A : PLAYER_SIG) (B : PLAYER_SIG) : sig
   (* Main engine of the game. returns the chip of the winning player *)
   val run_game : ?turn:chip -> board -> chip
 
-
 end = struct
-  (* method to print board for immersion *)
+  (* Method to print board for immersion *)
   let print_board (state : board) : unit =
     print_endline "GAME BOARD:";
   
@@ -19,7 +18,6 @@ end = struct
       match state.(0) with
       | Collumn (_, arr) -> Array.length arr
     in
-  
     (* Print each row from top (num_rows - 1) to bottom (0) *)
     for row = num_rows - 1 downto 0 do
       let line =
@@ -35,18 +33,42 @@ end = struct
       in
       print_endline line
     done
+
+  (* Method to check if last dropped chip was a winner *)
+  let check_win (_state : board) (_last_drop : int) : bool =
+    false
   
-    let rec game_helper (turn : chip) (state : board) : chip =
-    (* Get the collumn that the player will drop a chip at *)
-    let to_drop = 
-      match turn with
-      | A -> A.make_move state
-      | B -> B.make_move state
-      | _ -> failwith "tun can only be either a or b"
-    in
+  (* Main engine of game *)
+  let rec game_helper (turn : chip) (state : board) : chip =
+  (* Get next player to make a move and*)
+  (* Get the collumn that the player will drop a chip at *)
+  let to_drop, next_player = 
+    match turn with
+    | A -> A.make_move state, B
+    | B -> B.make_move state, A
+    | _ -> failwith "tun can only be either a or b"
+  in
+
+  let col_d = state.(to_drop) in
+  let Collumn(fill, current_col) = col_d in
+
+  (* If player tries to cheat and rop on already full collumn, opponent wins *)
+  if fill >= 6 then B else begin
+    (* Otherwise we drop chip into collumn *)
+    current_col.(fill) <- turn;
+    let new_collumn = Collumn(fill + 1, current_col) in
+    state.(to_drop) <- new_collumn;
     
-    (* Temporarily return -1 *)
-    A
+    (* Check if the last drop won the game *)
+    if check_win state to_drop then 
+    (* If last srop was a winner, player that dropped won the game *)
+      turn 
+    else
+    (* Otherwise let next player play *)
+      game_helper next_player state
+    
+
+  end
 
   let run_game ?(turn : chip = A) (state : board) : chip =
     (* Print inital game state *)
